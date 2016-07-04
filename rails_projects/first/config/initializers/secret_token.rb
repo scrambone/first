ab77e7247ff9a191a5ab2fa457a9dd62047b1826
scrambone <dmitro.federlyan@gmail.com>
2016-07-04 22:08:05 +0300

    Initialize 2 time

---
+++ rails_projects/first/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+First::Application.config.secret_key_base = 'bcfcbc745e22b1fcec6c0d19c353899dc5579c41d6ec78a1b7ff6f6ddec343306829c22e08171ab3a192dd5aa3f79b1b885988e3fddce052a96db635c157d242'
